357fde69236f43852dbdff261010eb07f0aa868c
Kacper Manterys <kacpermanterys@gmail.com>
2022-05-21 14:03:17 +0200

    Add for in list

--- testing.py
+++ testing.py
@@ -5,3 +5,8 @@ shopping = {
 print(shopping)
 
 shopping_dict = dict(shopping)
+
+for shop, product in shopping_dict.items():
+  print(f"Idę do {shop.capitalize()} i kupuję {product[0].capitalize()}, {product[1].capitalize()}, {product[2].capitalize()}")
+
+print(f"W sumie kupueję {3*2} produktów")
