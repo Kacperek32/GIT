fd68f41a67130467be081f6c96f40d6a378b34c2
Kacper Manterys <kacpermanterys@gmail.com>
2022-05-23 18:42:02 +0200

    Add whole project

--- testing.py
+++ testing.py
@@ -1,11 +1,13 @@
+print("Lista zakupów")
+
 shopping = {
     "piekarnia": ["bułki", "chleb", "pączek"],
     "warzywniak": ["marchew", "rukola", "seler"]
 }
-print(shopping)
+
+#iter test
 
 shopping_dict = dict(shopping)
-
 for shop, product in shopping_dict.items():
   print(f"Idę do {shop.capitalize()} i kupuję {product[0].capitalize()}, {product[1].capitalize()}, {product[2].capitalize()}")
 
