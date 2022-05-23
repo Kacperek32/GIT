bc2c88df0aef6c0cd07fa12fd8b839cff784a919
Kacper Manterys <kacpermanterys@gmail.com>
2022-05-23 18:49:42 +0200

    Add comment

--- testing.py
+++ testing.py
@@ -8,7 +8,10 @@ shopping = {
 #iter test
 
 shopping_dict = dict(shopping)
+
 for shop, product in shopping_dict.items():
   print(f"Idę do {shop.capitalize()} i kupuję {product[0].capitalize()}, {product[1].capitalize()}, {product[2].capitalize()}")
 
-print(f"W sumie kupueję {3*2} produktów")
+print(f"W sumie kupuję {3*2} produktów")
+
+# blue is my favorite colour
